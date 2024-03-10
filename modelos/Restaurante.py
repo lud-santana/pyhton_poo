cb318e431b941ec44b8b3f93078426e22c95eb01
lud-santana <yurisilvasantana2011@hotmail.com>
2024-03-10 20:09:15 -0300

    Testar a instância de objetos da classe Restaurante

--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -1,7 +1,15 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    estado = False
-
-    pizzaolo = Restaurante()
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
     
+    def __str__(self):
+        return f"{self.nome} - {self.categoria}"
+        
+
+restaurante_pizza = Restaurante("Pizza", "Italiano")
+restaurante_praca = Restaurante("Praça", "Gourmet")
+
+print(restaurante_pizza)
+print(restaurante_praca)
