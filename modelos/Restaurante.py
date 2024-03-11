136f0c3a863ec012b53863fcb6c448c611dce22a
lud-santana <yurisilvasantana2011@hotmail.com>
2024-03-10 22:43:49 -0300

    Estruturar o construtor para adicionar os objetos instanciados em uma lista e apresentar os objetos através de uma função

--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -1,15 +1,20 @@
 class Restaurante:
-    def __init__(self, nome, categoria):
+    restaurantes = []
+
+    def __init__(self, nome, categoria, ativo = False):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self.ativo = ativo
+        Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f"{self.nome} - {self.categoria}"
-        
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print("{} | {} | {}".format(restaurante.nome.ljust(5), restaurante.categoria.ljust(8), restaurante.ativo))
 
-restaurante_pizza = Restaurante("Pizza", "Italiano")
-restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante(nome = "Pizza", categoria = "Italiano")
+restaurante_praca = Restaurante(nome = "Praça", categoria = "Gourmet")
 
-print(restaurante_pizza)
-print(restaurante_praca)
+Restaurante.listar_restaurantes()
