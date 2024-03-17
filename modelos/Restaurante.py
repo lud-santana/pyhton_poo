afa61ed61aa889c778e14f87dafe697381d499c5
lud-santana <yurisilvasantana2011@hotmail.com>
2024-03-17 13:56:17 -0300

    Implementar property para modificar o retorno do status e aprimorar a listagem dos restaurantes usando as funções title() e upper()

--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -1,20 +1,25 @@
 class Restaurante:
     restaurantes = []
 
-    def __init__(self, nome, categoria, ativo = False):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = ativo
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f"{self.nome} - {self.categoria}"
     
     def listar_restaurantes():
+        print(f"\t{"Nome do restaurante".ljust(30)} \t{"Categoria".ljust(30)}  \t{"Status"}")
         for restaurante in Restaurante.restaurantes:
-            print("{} | {} | {}".format(restaurante.nome.ljust(5), restaurante.categoria.ljust(8), restaurante.ativo))
+            print(f"{restaurante._nome.ljust(35)} | {restaurante._categoria.ljust(35)} | {restaurante.ativo}")
 
-restaurante_pizza = Restaurante(nome = "Pizza", categoria = "Italiano")
-restaurante_praca = Restaurante(nome = "Praça", categoria = "Gourmet")
+    @property
+    def ativo(self):
+        return "✅" if self._ativo else "❌"
 
-Restaurante.listar_restaurantes()
+restaurante_pizza = Restaurante(nome = "pizza", categoria = "italiano")
+restaurante_praca = Restaurante(nome = "praça", categoria = "gourmet")
+
+Restaurante.listar_restaurantes() 
